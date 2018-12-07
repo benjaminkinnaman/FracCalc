dfa7cfe470cea82cb1547cd672ef45cd527d1030
Alayne and Paul <Alayne and Paul@10.0.0.2>
2018-12-06 22:24:24 -0800

    12/6 some broken part 2 code hopefully can fix :(

--- FracCalc/src/fracCalc/FracCalc.java
+++ FracCalc/src/fracCalc/FracCalc.java
@@ -33,51 +33,70 @@ public class FracCalc {
     //        
     // The function should return the result of the fraction after it has been calculated
     //      e.g. return ==> "1_1/4"
-/*    public static String produceAnswer(String input)
+   public static String produceAnswer(String input)
     { 
         // TODO: Implement this function to produce the solution to the input
-        String part1 = null;
+/*        String part1 = null;
         String operand = null;
-        String part2 = null;
-        String[] splitExpression = input.split(" ");
-        part1 = splitExpression[0];
-        operand = splitExpression[1];
-        part2 = splitExpression[2];
+        String part2 = null;	*/
+        String[] splitExpression = new String[3];
+        splitExpression = input.split(" ");
+        String part1 = splitExpression[0];
+        String operand = splitExpression[1];
+        String part2 = splitExpression[2];
+        String[] part2return = parseOperand(part2);
         //make return a string that includes part 2 
-                
+        return ("whole:" + part2return[0] + " numerator:" + part2return[1] + " denominator:" + part2return[2]);
+        
+        
      //return part2;		//was for part 1
-    }	*/
+    }	
  
     // TODO: Implement this function to produce the solution to the input
     
-    public static String produceAnswer(String input)
+    
+    
+   
+    
+/*    public static String produceAnswer(String input)
     { 
     	//Figure out what's going on in the input
     		//>Send each  number to parseOperand
     	//
     	return "Code incomplete";
     }
-    public static String[] parseOperand(String input) {
+    */
+    
+    
+    
+    
+    
+    public static String[] parseOperand(String input) {		//Makes the user input into an array the rest of the program can use
     	String[] parsed = {null, null, null};
     	if (input.contains("_") && input.contains("/")) {
     		//This is a fraction and a whole number
-    		
-    		//
-    		
+    		String[] preParsing = new String[2];
+    		preParsing = input.split("_");
+    		//Now we have to get the fraction out.  //Beware of spaghetti code here sorry ms dreyer
+    		String[] preParsing2 = (preParsing[1].split("/"));	//Seperates fraction
+    		parsed[0] = preParsing[0];				//Final assignment
+    		parsed[1] = preParsing2[0];				//Final assignment
+    		parsed[2] = preParsing2[1];				//Final assignment
+
     	} else if (input.contains("/")) {
     		//This is just a fraction
-    		String[] dissect = input.split("/");
-    		
+    		parsed = input.split("/");				//Final assignment
+    			//Returns like this: "[num,num]"
     		
     	} else {
-    		//This is just a whole number.
-    		
-    		
+    		//This is just a whole number.			//Throw if "num_" instead of "num" [DONE] 
+    		parsed = input.split("_");				//Final assignment
+    			//Returns like this: "[num]"
     	}
     	
     	
     	
-    	return parsed;//String[]
+    	return parsed;//String[]     //Might have to convert to regular string
     // TODO: Fill in the space below with any helper methods that you think you will need
     }
 }
